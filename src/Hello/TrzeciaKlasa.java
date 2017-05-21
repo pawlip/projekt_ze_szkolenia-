328094ade893a383ceab412909a859856b8bd677
Paweł Lipowski <pawlip29@gmail.com>
2017-05-21 12:35:27 +0200

    trzecia werja

---
+++ src/Hello/TrzeciaKlasa.java
@@ -0,0 +1,14 @@
+package Hello;
+
+public class TrzeciaKlasa {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		
+		System.out.println("bla;bla;bla");
+		
+
+	}
+
+}
